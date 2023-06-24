8aa34e8004ee3cf2f279176388d30022aa79d378
virghiu <virghiu@gmail.com>
2023-06-23 23:38:17 -0400

    feat: add generate number function

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+GENERATE_RANDOM_NUMBER() {
+  RANDOM_NUMBER=$(($RANDOM % 1000 + 1))
+  echo "$RANDOM_NUMBER"
+}
