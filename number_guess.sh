b456ceeaf600407190a4287b0f46d3b8e1e8b8c0
virghiu <virghiu@gmail.com>
2023-06-24 00:05:30 -0400

    feat: add prompt to get username and check if it exist in db.
    feat: add get username function
    
    fix: get user function

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,27 @@ GENERATE_RANDOM_NUMBER() {
   RANDOM_NUMBER=$(($RANDOM % 1000 + 1))
   echo "$RANDOM_NUMBER"
 }
+
+ # get user from databse
+GET_USERNAME() {
+  local QUERY="SELECT * FROM users WHERE username='$1'"
+  $PSQL "$QUERY"
+}
+
+
+MAIN() {
+  echo Enter your username:
+  read USERNAME
+
+ local GET_USER=$(GET_USERNAME "$USERNAME")  
+
+  if [[ -z $GET_USER ]]
+  then
+    echo Welcome, $USERNAME! It looks like this is your first time here.
+  else
+    echo Welcome back, $USERNAME
+  fi
+
+}
+
+MAIN
