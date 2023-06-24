a770bb7af7c33217fd735bd588286cb15a035e90
virghiu <virghiu@gmail.com>
2023-06-24 00:01:34 -0400

    fix: add db name in PSQL variable

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 GENERATE_RANDOM_NUMBER() {
   RANDOM_NUMBER=$(($RANDOM % 1000 + 1))
