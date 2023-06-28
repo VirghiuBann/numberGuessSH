bdb5699aeda5cfa743fd935a7dff7b91f99cd8aa
virghiu <virghiu@gmail.com>
2023-06-27 22:37:52 -0400

    feat: complete number guess feature.
    feat: add user if it doesn't exist in DB.
    
    feat: change get_username to get_user_id.
    
    feat: check if the imput number is valid.
    
    feat: add create guess function.
    
    feat: add feature  succeed guess number.
    
    feat: insert number of guesses.
    
    feat: succed number guess

--- number_guess.sh
+++ number_guess.sh
@@ -8,25 +8,80 @@ GENERATE_RANDOM_NUMBER() {
 }
 
  # get user from databse
-GET_USERNAME() {
-  local QUERY="SELECT * FROM users WHERE username='$1'"
+GET_USER_ID() {
+  local QUERY="SELECT user_id FROM users WHERE username='$1'"
   $PSQL "$QUERY"
+}
+
+#create an user
+CREATE_USER() {
+  local QUERY="INSERT INTO users(username) VALUES('$1')"
+  $PSQL "$QUERY"
+}
+
+#insert a guess
+INSERT_GUESS() {
+  local USER_ID=$1
+  local NUMBER_OF_GUESSES=$2
+  local QUERY="INSERT INTO guesses(user_id, number_of_guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)"
+  $PSQL "$QUERY"
+}
+
+GET_USER_GUESSES_DETAIL() {
+  local USER_ID=$1
+  local USERNAME=$2  
+
+  local GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM guesses WHERE user_id=$USER_ID")
+  local BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM guesses WHERE user_id=$USER_ID")
+  
+  echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 }
 
 
 MAIN() {
-  echo Enter your username:
-  read USERNAME
+  echo -e "\nEnter your username:"
+  read -r USERNAME
 
- local GET_USER=$(GET_USERNAME "$USERNAME")  
+ local USER_ID=$(GET_USER_ID "$USERNAME")  
 
-  if [[ -z $GET_USER ]]
+  if [[ -z $USER_ID ]]
   then
-    echo Welcome, $USERNAME! It looks like this is your first time here.
+    echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+    CREATE_USER_RESULT=$(CREATE_USER "$USERNAME")   
+    USER_ID=$(GET_USER_ID "$USERNAME")
   else
-    echo Welcome back, $USERNAME
+    GET_USER_GUESSES_DETAIL "$USER_ID" "$USERNAME"
   fi
 
+  SECRET_NUMBER_GUESS=$(GENERATE_RANDOM_NUMBER)
+  echo "Guess the secret number between 1 and 1000:" 
+  # try conuter guesses
+  NUMBER_TRIES=0
+
+  while true
+  do         
+    read -r USER_NUMBER_GUESS
+    
+    if [[ ! $USER_NUMBER_GUESS =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+    else
+      NUMBER_TRIES=$((NUMBER_TRIES + 1)) 
+    
+      if [[ $USER_NUMBER_GUESS -eq $SECRET_NUMBER_GUESS  ]]
+      then
+        INSERT_GUESS_RESULT=$(INSERT_GUESS "$USER_ID" "$NUMBER_TRIES")
+        
+        echo -e "You guessed it in $NUMBER_TRIES tries. The secret number was $SECRET_NUMBER_GUESS. Nice job!"
+        break
+      elif [[ $USER_NUMBER_GUESS -gt $SECRET_NUMBER_GUESS  ]]
+      then 
+        echo "It's higher than that, guess again:"
+      else
+        echo "It's lower than that, guess again:"
+      fi      
+    fi
+  done
 }
 
 MAIN
